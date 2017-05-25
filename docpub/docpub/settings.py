b413e3916efbf27632edf323dc1033ac3ec17b65
Greg Linch <glinch@mcclatchydc.com>
2017-05-25 17:10:27 -0400

    test urls and view, updated settings

--- docpub/docpub/settings.py
+++ docpub/docpub/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
