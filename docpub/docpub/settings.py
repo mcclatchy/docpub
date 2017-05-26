acc59f9c5e88a0cce542cd522f9c771e9be98ebe
Greg Linch <glinch@mcclatchydc.com>
2017-05-26 10:03:18 -0400

    add docs app config to settings

--- docpub/docpub/settings.py
+++ docpub/docpub/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'docs.apps.DocsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
