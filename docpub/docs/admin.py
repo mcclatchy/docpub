a21432af83ecf0bd566e4dc7283bc0d3ce1e3c40
Greg Linch <glinch@mcclatchydc.com>
2017-05-26 10:02:51 -0400

    add document and basic info models + document admin

--- docpub/docs/admin.py
+++ docpub/docs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+
+class DocumentAdmin(admin.ModelAdmin):
+    fields = ['title', 'description', 'source', 'file', 'link']
+    list_display = ['title', 'source']
+
+## TEMPLATE
+# class Admin(admin.ModelAdmin):
+#     fields = ['']
+#     list_display = 
+#     # list_editable = ['']
+#     list_filter = 
+#     search_fields = 
+#     # exclude  = ['']
+
+
+admin.site.register(Document, DocumentAdmin)
+## TEMPLATE
+# admin.site.register(, )
