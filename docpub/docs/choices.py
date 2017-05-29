e6d634689079edf5233573906dc4cbaf388f74bc
Greg Linch <glinch@mcclatchydc.com>
2017-05-29 15:25:14 -0400

    add choices for access perms

---
+++ docpub/docs/choices.py
@@ -0,0 +1,8 @@
+## choices for models
+
+
+ACCESS_CHOICES = (
+    #('private', 'Private (only viewable by those with permission to this doc)'),
+    ('public', 'Public (viewable by anyone)'),
+    ('organization', 'Organization (viewable by users in your organization)')
+)
