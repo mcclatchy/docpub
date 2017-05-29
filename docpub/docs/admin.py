5c95fe413c7b7a09ae6bdabd115cf7dbad7385f8
Greg Linch <glinch@mcclatchydc.com>
2017-05-29 15:24:35 -0400

    update admin to include some of the new fields

--- docpub/docs/admin.py
+++ docpub/docs/admin.py
@@ -3,17 +3,30 @@ from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
-    fields = ['title', 'description', 'source', 'file', 'link']
-    list_display = ['title', 'source']
+    fields = ('documentcloud_id', 'title', ('file', 'link',), 'description', 'source', 'access', 'project', 'embed_code') # 'format_embed_code'
+    list_display = ('title', 'source', 'access', 'project','document_admin_url') ## copy_embed_code
+    list_filter = ('access', 'project')
+    readonly_fields = ('embed_code', 'documentcloud_id',)
+    actions = ('generate_embed_codes')
+
+    ## add admin action to generate document embed code list for user
+    def generate_embed_codes(self, request, queryset):
+        message = ''
+        for document in queryset:
+            message += str(document.embed_code)
+            # message.append(race.embed_code)
+        self.message_user(request, '%s' % message)
+    generate_embed_codes.short_description = 'Get embed codes for selected documents'
+
 
 ## TEMPLATE
 # class Admin(admin.ModelAdmin):
-#     fields = ['']
+#     fields = ('')
 #     list_display = 
-#     # list_editable = ['']
+#     # list_editable = ('')
 #     list_filter = 
 #     search_fields = 
-#     # exclude  = ['']
+#     # exclude  = ('')
 
 
 admin.site.register(Document, DocumentAdmin)
