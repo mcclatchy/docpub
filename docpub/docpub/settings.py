775c1ad93f17f55b5b68518040e1b6ccbb142f05
Greg Linch <glinch@mcclatchydc.com>
2017-06-06 16:28:44 -0400

    move allowed hosts to private settings

--- docpub/docpub/settings.py
+++ docpub/docpub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from .settings_private import SECRET_KEY
+from .settings_private import SECRET_KEY, ALLOWED_HOSTS
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [] ## imported from private
 # INTERNAL_IPS = ['127.0.0.1']
 
 DOMAIN = os.path.join(BASE_DIR, '')
