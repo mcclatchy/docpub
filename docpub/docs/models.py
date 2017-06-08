0dfc36c0f62cf93ec7b69a845291bfdb36512ac1
Greg Linch <glinch@mcclatchydc.com>
2017-06-08 16:23:19 -0400

    add newsroom and uploaded by, remove unneeded commented out code, comment out doccloud cred model

--- docpub/docs/models.py
+++ docpub/docs/models.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.utils.html import format_html
 from django.contrib.auth.models import User
 from docs.connection import client
-from docs.choices import ACCESS_CHOICES
+from docs.choices import ACCESS_CHOICES, NEWSROOM_CHOICES
 from docpub.settings import DOMAIN, UPLOAD_PATH
-from docpub.settings_private import CDN_DOMAIN
-# import boto3
 
 
 ##### DOCUMNET METHODS #####
@@ -51,6 +49,7 @@ def document_upload(self):
         'access': self.access,
         'secure': self.secure,
         'project': self.get_project_object(),
+        # 'data': ,
     }
     obj = client.documents.upload(pdf, **kwargs)
     self.documentcloud_id = obj.id
@@ -59,39 +58,12 @@ def document_upload(self):
 ## generate/update embed code
 def generate_embed(self):
     doc_id = self.documentcloud_id
-    # doc_title = self.title
-    # doc_id_number = doc_id.split('-')[0]
-    # doc_title_hyphenated = doc_title.replace(' ', '-')
     doc_sidebar = str(self.sidebar).lower()
-    # doc_thumb = self.documentcloud_thumbnail
-    # if not doc_thumb:
-        # doc_thumb = ''
-    # doc_pdf = self.documentcloud_pdf_url
-    ## styles for hiding Document Viewer on native apps and showing thumbnail
-    # script = '<script>if (!window.jQuery){var embeds=document.getElementsByClassName("doccloud"); var thumbs=document.getElementsByClassName("docthumb"); for (var i=0;i<embeds.length;i++){embeds[i].style.display="none";} for (var i=0;i<thumbs.length;i++){thumbs[i].style.display="inline";}}</script>'
-    # embed_prefix = '<style>#DV-viewer-{id} {{ display: inline; }} .docthumb {{ display: none; }}</style><div class="docthumb"><a href="{pdf}"><img src="{thumb}" width="100%" /></a></div>'.format(
-        #     id=doc_id,
-        #     pdf=doc_pdf,
-        #     thumb=doc_thumb
-        # )
-    ## construct the DocumentCloud embed code wrapped in div we'll hide for apps
-    # standard_embed = '<div class="doccloud"><div id="DV-viewer-{id}" class="DC-embed DC-embed-document DV-container"></div><script src="//assets.documentcloud.org/viewer/loader.js"></script><script>DV.load("https://www.documentcloud.org/documents/{id}.js", {{responsive: true, sidebar: {sidebar}, container: "#DV-viewer-{id}"}});</script></div><noscript><a href="https://assets.documentcloud.org/documents/{id_number}/{title_hyphenated}.pdf">{title} (PDF)</a><br /><a href="https://assets.documentcloud.org/documents/{id_number}/{title_hyphenated}.txt">{title} (Text)</a></noscript>'.format(
-    #         id=doc_id,
-    #         title=doc_title,
-    #         id_number=doc_id_number, 
-    #         title_hyphenated=doc_title_hyphenated, 
-    #         sidebar=doc_sidebar
-    #     )
     style_height = '<style>@media (max-width: 420px) iframe {height: 500px;}</style>'
     iframe_embed = '<div><iframe src="https://www.documentcloud.org/documents/{id}.html?sidebar={sidebar}" style="border:none;width:100%;height:500px"></iframe></div>'.format(
             id=doc_id,
             sidebar=doc_sidebar
-            # title=doc_title, 
-            # id_number=doc_id_number, 
-            # title_hyphenated=doc_title_hyphenated, 
         ) # desktop height 930px, mobile height 500px
-
-    # self.embed_code = script + embed_prefix + standard_embed
     self.embed_code = style_height + iframe_embed
 
 
@@ -118,13 +90,15 @@ class Document(BasicInfo):
         - You can only use one PDF option (upload or link)<br> \
         - The PDF cannot be changed or updated after you hit save for the first time &mdash; no matter which way you add it.')
     messy_text = models.BooleanField(default=False, help_text='Check this box if the "Plain text" is too messy to include or clean up manually. A link to the PDF will be displayed on mobile with no plain text version.')
+    newsroom = models.CharField(max_length=255, null=True, blank=True, choices=NEWSROOM_CHOICES)
     project = models.CharField(max_length=255, null=True, blank=True, choices=build_project_list(), help_text='Optional, but helpful. Cannot be updated from here after initially set -- must be changed in DocumentCloud.') ## UPDATE: remove blank=True to make required?
-    text = models.TextField(null=True, blank=True, verbose_name='Document text', help_text='Text of the PDF extracted by DocumentCloud. Leave this blank when you first upload the document. It will be filled in automatically. If the plain text does not appear after initially creating/saving here, check on DocumentCloud.org to see if the document is finished processing. When it is done, come back here and click "Save and continue editing" below, then verify the text is filled in here. If you would like, you can clean up the text as needed after it appears here. At any point in the process, the plain text will not be overwritten if there is any text here -- original or modified.')
-    title = models.CharField(max_length=255, blank=False, null=True, help_text='Short yet descriptive title (e.g. 2017 House budget proposal). <strong>Public</strong>')
     related_article = models.URLField(max_length=255, blank=True, null=True, help_text='Optional link to the story this document relates to. <strong>Public</strong>')
     secure = models.BooleanField(blank=True, default=False, help_text='Is this document sensitive or should it not be sent to third-party services (e.g. OpenCalais for text analysis)?')
     sidebar = models.BooleanField(blank=True, default=False, verbose_name='Enable document viewer sidebar?', help_text='Not recommended for article page embeds.')
     source = models.CharField(max_length=255, blank=True, null=True, verbose_name='Source name', help_text='What organization, person, etc. created this document? Optional, but strongly encouraged if not a senstive/confidential. <strong>Public</strong>')
+    text = models.TextField(null=True, blank=True, verbose_name='Document text', help_text='Text of the PDF extracted by DocumentCloud. Leave this blank when you first upload the document. It will be filled in automatically. If the plain text does not appear after initially creating/saving here, check on DocumentCloud.org to see if the document is finished processing. When it is done, come back here and click "Save and continue editing" below, then verify the text is filled in here. If you would like, you can clean up the text as needed after it appears here. At any point in the process, the plain text will not be overwritten if there is any text here -- original or modified.')
+    title = models.CharField(max_length=255, blank=False, null=True, help_text='Short yet descriptive title (e.g. 2017 House budget proposal). <strong>Public</strong>')
+    uploaded_by = models.CharField(max_length=255, null=True, blank=True)
 
     def documentcloud_url_formatted(self):
         link = '-'
@@ -154,6 +128,7 @@ class Document(BasicInfo):
             'secure': self.secure,
             'related_article': self.related_article,
             'project': self.get_project_object(),
+            # 'data': ,
         }
 
     ## create a button in the admin listview for users to copy a specific embed code
@@ -191,3 +166,18 @@ class Document(BasicInfo):
     ## we'd also want to grab doc text and thumbnail
     # obj.full_text
     # obj.pdf_url
+
+
+# class DocumentCloudCredentials(BasicInfo):
+#     # email = models.EmailField(max_length=254, help_text='Email address for user in DocPub must be the same as DocumentCloud email address.')
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     password = models.CharField(max_length=255, verbose_name='DocumentCloud password')
+
+#     class Meta:
+#         ordering = ['-created'] # updated might get confusing, but could be more helpful
+#         verbose_name = 'DocumentCloud login'
+
+#     def __str__(self):
+#         return self.user.email
+
+
