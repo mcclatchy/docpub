86f1e4ae731303db4328949b15ce6126fc2354fd
Greg Linch <glinch@mcclatchydc.com>
2017-06-08 14:44:24 -0400

    add commented out possible abstraction for using individual account

--- docpub/docs/connection.py
+++ docpub/docs/connection.py
@@ -1,5 +1,16 @@
 from documentcloud import DocumentCloud
 from docpub.settings_private import DC_USERNAME, DC_PASSWORD
+# from .models import DocumentCloudCredentials
 
 
-client = DocumentCloud(DC_USERNAME, DC_PASSWORD)
+# documentcloud_password = DocumentCloudCredentials.password
+
+# if not documentcloud_password:
+username = DC_USERNAME
+password = DC_PASSWORD
+
+client = DocumentCloud(username, password)
+
+# def client(username, password):
+#     client = DocumentCloud(username, password)
+#     return client
