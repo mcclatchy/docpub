cf3deceeeb9b51b2a887f234f8a96f2f4b2f7e3f
Greg Linch <glinch@mcclatchydc.com>
2017-06-09 14:28:32 -0400

    update help text, including uppercasing all PUBLIC labels

--- docpub/docs/models.py
+++ docpub/docs/models.py
@@ -78,7 +78,7 @@ class BasicInfo(models.Model):
 
 class Document(BasicInfo):
     access = models.CharField(max_length=255, null=True, choices=ACCESS_CHOICES, verbose_name='Who can see this?', help_text='Should the document be publicly visible or only visible to other users in your DocumentCloud organization?')
-    description = models.TextField(blank=True, null=True, help_text='Optional (but strongly encouraged) description of the document. <strong>Public</strong>')
+    description = models.TextField(blank=True, null=True, help_text='Optional (but strongly encouraged) description of the document. <strong>PUBLIC</strong>')
     documentcloud_id = models.CharField(max_length=255, null=True, blank=True, verbose_name='DocumentCloud ID', help_text='ID of the document on DocumentCloud')
     documentcloud_pdf_url = models.URLField(max_length=255, blank=True, null=True, verbose_name='PDF hosted by DocumentCloud', help_text='Automatically pulled from DocumentCloud after document finishes processing.')
     # documentcloud_thumbnail = models.URLField(max_length=255, blank=True, null=True, verbose_name='Document thumbail', help_text='Pulled from DocumentCloud after document finishes processing.')
@@ -88,17 +88,17 @@ class Document(BasicInfo):
     link = models.URLField(max_length=255, null=True, blank=True, verbose_name='Link to PDF', help_text='...paste the URL of a PDF you would like to upload. \
         <br><strong>*** NOTES ***</strong><br> \
         - You can only use one PDF option (upload or link)<br> \
-        - The PDF cannot be changed or updated after you hit save for the first time &mdash; no matter which way you add it.')
+        - The PDF cannot be changed or updated after you hit save for the first time &mdash; no matter which way you add it. <strong>PUBLIC</strong>')
     messy_text = models.BooleanField(default=False, help_text='Check this box if the "Plain text" is too messy to include or clean up manually. A link to the PDF will be displayed on mobile with no plain text version.')
-    newsroom = models.CharField(max_length=255, null=True, blank=True, choices=NEWSROOM_CHOICES)
+    newsroom = models.CharField(max_length=255, null=True, blank=True, choices=NEWSROOM_CHOICES, help_text='If left blank, it will set the newsroom based on the domain of the email account you use for this tool.')
     project = models.CharField(max_length=255, null=True, blank=True, choices=build_project_list(), help_text='Optional, but helpful. Cannot be updated from here after initially set -- must be changed in DocumentCloud.') ## UPDATE: remove blank=True to make required?
-    related_article = models.URLField(max_length=255, blank=True, null=True, help_text='Optional link to the story this document relates to. <strong>Public</strong>')
+    related_article = models.URLField(max_length=255, blank=True, null=True, help_text='Optional link to the story this document relates to. <strong>PUBLIC</strong>')
     secure = models.BooleanField(blank=True, default=False, help_text='Is this document sensitive or should it not be sent to third-party services (e.g. OpenCalais for text analysis)?')
-    sidebar = models.BooleanField(blank=True, default=False, verbose_name='Enable document viewer sidebar?', help_text='Not recommended for article page embeds.')
-    source = models.CharField(max_length=255, blank=True, null=True, verbose_name='Source name', help_text='What organization, person, etc. created this document? Optional, but strongly encouraged if not a senstive/confidential. <strong>Public</strong>')
+    sidebar = models.BooleanField(blank=True, default=False, verbose_name='Enable document viewer sidebar?', help_text='Not recommended for article page embeds. This really only works well when used with a full-width template.')
+    source = models.CharField(max_length=255, blank=True, null=True, verbose_name='Source name', help_text='What organization, person, etc. created this document? Optional, but strongly encouraged if not a senstive/confidential. <strong>PUBLIC</strong>')
     text = models.TextField(null=True, blank=True, verbose_name='Document text', help_text='Text of the PDF extracted by DocumentCloud. Leave this blank when you first upload the document. It will be filled in automatically. If the plain text does not appear after initially creating/saving here, check on DocumentCloud.org to see if the document is finished processing. When it is done, come back here and click "Save and continue editing" below, then verify the text is filled in here. If you would like, you can clean up the text as needed after it appears here. At any point in the process, the plain text will not be overwritten if there is any text here -- original or modified.')
-    title = models.CharField(max_length=255, blank=False, null=True, help_text='Short yet descriptive title (e.g. 2017 House budget proposal). <strong>Public</strong>')
-    uploaded_by = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, blank=False, null=True, help_text='Short yet descriptive title (e.g. 2017 House budget proposal). <strong>PUBLIC</strong>')
+    uploaded_by = models.CharField(max_length=255, null=True, blank=True, help_text='If left blank, it will grab the first part of your email address (specifically, everything before the @ symbol).')
 
     def documentcloud_url_formatted(self):
         link = '-'
