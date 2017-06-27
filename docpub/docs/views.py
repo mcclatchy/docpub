8fcf56baf6d934f46302dfb447b5fdf9d564863f
Greg Linch <glinch@mcclatchydc.com>
2017-06-27 14:18:30 -0400

    clean up views for only including login index page for now

--- docpub/docs/views.py
+++ docpub/docs/views.py
@@ -3,14 +3,9 @@ from django.template.context import RequestContext
 
 
 def index(request):
-   context = {'user': request.user}
-   return render(request, 'index.html', context)
-
-
-# from django.shortcuts import render
-# from django.http import HttpResponse
-
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        'request': request,
+        'user': request.user
+    }
+    return render(request, 'index.html', context)
 
