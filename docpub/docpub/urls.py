ce3da6445757877335ee0aef95890186c8815a45
Greg Linch <glinch@mcclatchydc.com>
2017-07-13 15:05:35 -0400

    add contl to not include env if it's prod

--- docpub/docpub/urls.py
+++ docpub/docpub/urls.py
@@ -16,12 +16,19 @@ urlpatterns = [
     url(r'^s3direct/', include('s3direct.urls')),
 ]
 
+## if Django is in debug mode...
 if DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 
-admin.site.site_header = 'DocPub {}'.format(DOCPUBENV)
+## if we're not on prod, include the environment in the header
+if DOCPUBENV != 'prod':
+    env = DOCPUBENV
+
+admin.site.site_header = 'DocPub {}'.format(env)
+
+## disable the 'View site' in the admin header
 admin.site.site_url = None
 
