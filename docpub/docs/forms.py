2a7dfa99d36df9e8f378a09776640c5b3bf48664
Greg Linch <glinch@mcclatchydc.com>
2017-09-07 18:14:25 -0400

    add verified and encrypted to doccloudcreds form

--- docpub/docs/forms.py
+++ docpub/docs/forms.py
@@ -6,7 +6,7 @@ class PasswordInline(ModelForm):
 
     class Meta:
         model = DocumentCloudCredentials
-        fields = ('password', 'newsroom')
+        fields = ('password', 'newsroom', 'verified', 'encrypted')
         widgets = {
             'password': PasswordInput(render_value=True),
         }
